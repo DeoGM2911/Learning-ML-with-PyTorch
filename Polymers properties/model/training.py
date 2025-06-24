85772a3e260c0c1fba6547d89370e64fbb0b981e
Dung Tran <119715508+DeoGM2911@users.noreply.github.com>
2025-06-24 10:05:55 +0700

    Update training.py

--- Polymers properties/model/training.py
+++ Polymers properties/model/training.py
@@ -3,16 +3,17 @@ from torch import optim
 from model.data import PolymerDataset
 import matplotlib.pyplot as plt
 from utils.visualize import visualize_loss
+from sklearn.utils import shuffle
 
 
 def configure(model, device, loss_func, lr):
     model.to(device)
-    criterion = loss_func
+    criterion = loss_func()
     optimizer = optim.Adam(model.parameters(), lr=lr)
     return criterion, optimizer
 
 
-def train(models, optimizers, criterions, dataset: PolymerDataset, cnn_model, rnn_model, num_epochs):
+def train(models, optimizers, criterions, dataset: PolymerDataset, cnn_model, rnn_model, num_epochs, batch_size):
     properties = ["tg", "tc", "ffv", "density", "rg"]
     # To record the loss history
     train_history = {p: np.zeros(num_epochs) for p in properties}
@@ -25,29 +26,44 @@ def train(models, optimizers, criterions, dataset: PolymerDataset, cnn_model, rn
             model.train()
             optimizers[property].zero_grad()
             
-            # Forward pass
-            y_hat = model(dataset.get_features(property, True, cnn_model, rnn_model))
-            train_loss = criterions[property](y_hat, dataset.get_targets(property))
-            
-            # Backward step
-            train_loss.backward()
-            optimizers[property].step()
+            X_train = dataset.get_features(property, True, cnn_model, rnn_model)
+            y_train = dataset.get_targets(property)
+            # Shuffle the data
+            X_train, y_train = shuffle(X_train, y_train)
+            N_train = int(np.ceil(len(y_train) // batch_size))
+            train_loss_epoch = []
+            for i in range(N_train):
+                end = min((i+1) * batch_size, len(y_train))
+                X_batch, y_batch = X_train[i * batch_size: end], y_train[i * batch_size: end]
+                
+                # Forward pass
+                y_hat = model(X_batch)
+                train_loss = criterions[property](y_hat, y_batch)
+                
+                # Backward step
+                train_loss.backward()
+                optimizers[property].step()
+                train_loss_epoch.append(train_loss.item())
             
             # Record the train loss
-            train_history[property][epoch] = train_loss.item()
+            train_history[property][epoch] = np.mean(train_loss_epoch).item()
 
             # Validate
             model.eval()
-            y_hat = model(dataset.get_features(property, False, cnn_model, rnn_model))
-            test_loss = criterions[property](y_hat, dataset.get_targets(property, False))
-            test_history[property][epoch] = test_loss.item()
+            X_test = dataset.get_features(property, False, cnn_model, rnn_model)
+            y_test = dataset.get_targets(property, False)
+            N_test = int(np.ceil(len(y_test) // batch_size))
+            test_loss_epoch = []
+            for i in range(N_test):
+                end = min((i+1) * batch_size, len(y_test))
+                X_batch, y_batch = X_test[i * batch_size: end], y_test[i * batch_size: end]
+                y_hat = model(X_batch)
+                test_loss = criterions[property](y_hat, y_batch)
+                test_loss_epoch.append(test_loss.item())
+            
+            test_history[property][epoch] = np.mean(test_loss_epoch).item()
         
         # For keeping track of time
         print(f"Epoch: {epoch}")
-        
-        # Every 5 epochs, show the loss progess:
-        if epoch % 5 == 0 and epoch != 0:
-            plt.close()
-            visualize_loss(train_history, test_history)
         
     return train_history, test_history
