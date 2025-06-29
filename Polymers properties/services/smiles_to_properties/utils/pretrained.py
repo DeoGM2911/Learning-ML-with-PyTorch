5e036a06b06477021db0e501d1a6bd0219199cab
Dung Tran <dung20042911@gmail.com>
2025-06-29 15:16:20 +0700

    Add methods for load pretrained models

--- Polymers properties/services/smiles_to_properties/utils/pretrained.py
+++ Polymers properties/services/smiles_to_properties/utils/pretrained.py
@@ -1,2 +1,20 @@
+import torch
+from model.models import PolymerCNN
+
+
 def load_pretrained():
-    ...
+    # CNN models
+    tg_cnn = PolymerCNN(1, (32, 32, 64, 64, 128, 256), ((4, 8), (2, 4, 8, 8)), 2, (2, 4), 256)
+    ffv_cnn = PolymerCNN(1, (32, 64, 64, 128, 128, 256), ((4, 8), (8, 16, 16, 32)), 2, (2, 4), 512)
+    tc_cnn = PolymerCNN(1, (16, 32, 32, 64, 128, 256), ((4, 8), (2, 4, 8, 8)), 2, (2, 4), 256)
+    density_cnn = PolymerCNN(1, (32, 32, 64, 64, 128, 256), ((4, 8), (2, 4, 8, 8)), 2, (2, 4), 256)
+    rg_cnn = PolymerCNN(1, (16, 32, 32, 64, 128, 256), ((4, 8), (2, 4, 8, 8)), 2, (2, 4), 256)
+    
+    models = {'tg': tg_cnn, 'ffv': ffv_cnn, 'tc': tc_cnn, 'density': density_cnn, 'rg': rg_cnn}
+    device = torch.device("cuda:0" if torch.cuda.is_available() else 'cpu')
+    # Load in the models
+    for property in models:
+        model = models[property]
+        model.load_state_dict(torch.load(f'{property}_cnn.pt', map_location=device))
+    
+    return models
