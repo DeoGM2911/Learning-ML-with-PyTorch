68d74d75bc4383d4adb15b0c3923dcfcc267c910
Dung Tran <dung20042911@gmail.com>
2025-07-01 23:10:35 +0700

    Finish testing all models

--- Polymers properties/services/test/integrate_test.py
+++ Polymers properties/services/test/integrate_test.py
@@ -1,11 +1,7 @@
-import sys
-from pathlib import Path
-sys.path.append(str(Path(__file__).resolve().parent.parent))
-
-from smiles_to_properties.utils.pretrained import load_pretrained, predict
-from smiles_to_properties.utils.vectorize import mol_vectorize, EMBED_DIM
-from images_to_smiles.models.decimer import predict_smiles, is_chemical_image
-from images_to_smiles.utils.validate_txt import is_smiles_polymer
+from ..smiles_to_properties.utils.pretrained import load_pretrained, predict
+from ..smiles_to_properties.utils.vectorize import mol_vectorize, EMBED_DIM
+from ..images_to_smiles.models.decimer import predict_smiles, is_chemical_image
+from ..images_to_smiles.utils.validate_txt import is_smiles_polymer
 from pathlib import Path
 
 
@@ -14,16 +10,15 @@ if __name__ == "__main__":
     models = load_pretrained()
     
     # Convert the image into smiles if possible, else ignore
-    current = Path(__file__).resolve()
-    dest = Path(__file__).resolve().parent / "test_outputs"
-    imgs = current.parent.parent / "images_to_smiles" / "images"
+    imgs = Path(__file__).resolve().parent.parent / "images_to_smiles" / "images"
+    dest = Path(__file__).resolve().parent / "test_outputs" 
     for img in imgs.iterdir():
-        if is_chemical_image(img):
+        if is_chemical_image(img)[0]:
             smiles = predict_smiles(img)
             if is_smiles_polymer(smiles):
                 # Generate prediction
                 X = mol_vectorize([smiles], EMBED_DIM)
-                with open(str(dest / f"{str(img).split('.')[0]}.txt"), "w") as f:
+                with open(f"{str(dest)}/{img.stem}.txt", "w") as f:
                     f.write(str(predict(models, X)))
             else:
                 print("Not a polymer!")
